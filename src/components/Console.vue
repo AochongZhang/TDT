32d729c3b871eb2b8e36c3bf06057d171c698691
AochongZhang <aochongzhang@gmail.com>
2020-01-09 15:55:08 +0800

    修改可调高度

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="console">
-    <el-container :style="{height: containerHeight + 'px'}">
-      <el-header height="30px">
+    <div :style="{height: containerHeight + 'px'}">
+      <div class="title-bar">
         <el-row type="flex" justify="space-between">
           <el-col :span="12">
             <el-input v-model="host" size="mini">
@@ -17,13 +17,17 @@
             <el-button size="mini">连接</el-button>
           </el-col>
         </el-row>
-      </el-header>
-      <el-main class="main">
-        <el-container class="container">
-          <el-header height="30px" class="header">a</el-header>
-          <el-main class="main">b</el-main>
-        </el-container>
-        <!-- <div class="control-bar">
+      </div>
+
+      <div class="content-top" :style="{height: contentTopHeight + 'px'}"></div>
+      <div class="drag-line" @mousedown="dragLineMouseDown"></div>
+      <div class="content-bottom" :style="{height: contentBottomHeight + 'px'}"></div>
+    </div>
+
+    <!-- <el-scrollbar>
+              <el-input :autosize="{ minRows: 15}" resize="none" type="textarea" v-model="textarea"></el-input>
+    </el-scrollbar>-->
+    <!-- <div class="control-bar">
           <el-row type="flex" justify="space-between">
             <el-col :span="12">
               <el-switch v-model="value" active-color="#1c8bf0" inactive-color="#ccc"></el-switch>
@@ -38,14 +42,9 @@
         </div>
         <el-scrollbar class="control-text" :style="{height: textWrapHeight + 'px'}">
           <el-input :autosize="{ minRows: 15}" resize="none" type="textarea" v-model="textarea"></el-input>
-        </el-scrollbar>-->
-      </el-main>
-      <el-main class="main">
-        <el-container class="container">
-          <el-header height="30px" class="header">a</el-header>
-          <el-main class="main">b</el-main>
-        </el-container>
-        <!-- <div class="control-bar">
+    </el-scrollbar>-->
+
+    <!-- <div class="control-bar">
           <el-row type="flex" justify="space-between">
             <el-col :span="12">
               <el-switch v-model="value" active-color="#1c8bf0" inactive-color="#ccc"></el-switch>
@@ -58,9 +57,7 @@
             </el-col>
           </el-row>
         </div>
-        <el-input class="control-text" :style="{height: textWrapHeight + 'px'}" resize="none" type="textarea" :rows="5" v-model="textarea"></el-input>-->
-      </el-main>
-    </el-container>
+    <el-input class="control-text" :style="{height: textWrapHeight + 'px'}" resize="none" type="textarea" :rows="5" v-model="textarea"></el-input>-->
     <!-- <div class="title">
       <el-row type="flex" justify="space-between">
         <el-col :span="12">
@@ -128,13 +125,51 @@ export default {
       localPort: "",
       value: false,
       textarea: "aaaaaa",
-      global: this.Global
+      global: this.Global,
+      dragListMouseY: 0,
+      contentTopHeight: 0,
+      contentHeightMin: 100
     };
   },
   computed: {
     containerHeight() {
       return this.global.client.height - 40;
+    },
+    contentBottomHeight() {
+      return this.containerHeight - 40 - this.contentTopHeight;
     }
+  },
+  watch: {
+    // 窗口高度改变时，修改contentTopHeight高度
+    containerHeight(oldVal, newVal) {
+      this.contentTopHeight += ((oldVal - newVal) / 2)
+    },
+    // 限制contentTopHeight高度范围
+    contentTopHeight(val) {
+      let max = this.containerHeight - 40 - this.contentHeightMin
+      val = val < this.contentHeightMin ? this.contentHeightMin : val;
+      val = val > max ? max : val;
+      this.contentTopHeight = val;
+    }
+  },
+  methods: {
+    dragLineMouseDown(e) {
+      //阻止默认事件
+      e.preventDefault();
+      this.dragListMouseY = e.clientY - 50 - this.contentTopHeight;
+      document.onmousemove = this.dragLineMouseMove;
+      document.onmouseup = this.dragLineMouseUp;
+    },
+    dragLineMouseMove(e) {
+      this.contentTopHeight = e.clientY - 50 - this.dragListMouseY;
+    },
+    dragLineMouseUp() {
+      document.onmousemove = null;
+      document.onmouseup = null;
+    }
+  },
+  mounted() {
+    this.contentTopHeight = (this.containerHeight - 40) / 2
   }
 };
 </script>
@@ -170,6 +205,18 @@ export default {
 }
 
 .container {
-  height: 100%;;
+  height: 100%;
+}
+
+.drag-line {
+  height: 10px;
+  cursor: row-resize;
+}
+
+/* .drag-line, */
+.content-top,
+.content-bottom {
+  box-sizing: border-box;
+  border: 1px solid #575757;
 }
 </style>
